1f6b3f31a8028ec9446a83be6243ee4bcd103884
T-taku <t-takuscratch@outlook.jp>
2022-01-13 23:59:23 +0900

    🎉Release v0.1!

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import discord as d
 import discord
 import re
-from discord.ext import commands, tasks
-from typing import Union
+from discord.ext import tasks
 from discord.commands import Option
 import datetime
-import time as timetime
 import sqlite3
-from typing import List
 import asyncio
+from discord import enums as en
+import configparser
 
 bot = discord.Bot()
+
+def get_token(name):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    tokens = 'tokens'
+    return config.get(tokens, name)
 
 dbname = 'database/plan.db'
 bot.conn = sqlite3.connect(dbname)
@@ -22,6 +27,8 @@ bot.conn.commit()
 
 @bot.event
 async def on_ready():
+    listening = discord.Game("予定を通知します!")
+    await bot.change_presence(status=discord.Status.online, activity=listening)
     print("Login!")
 
 
@@ -50,13 +57,14 @@ def strptime(time):
     raise ValueError("不正な時刻です。")
 
 
-@bot.slash_command(guild_ids=[737154037937340506])
+@bot.slash_command(guild_ids=[int(get_token("guild_id"))])
 async def create(
     ctx,
     plan_name: Option(str, "予定の名前"),
+    mention: Option(en.SlashCommandOptionType(9), "メンション先"),
     date: Option(str, "日付(例:2022/01/01) ※半角数字のみ、1などの単数は01と入力"),
+    time: Option(str, "通知時刻を設定できます。(例:午後12時00分や、22:00なども可能)※半角数字のみ、1などの単数は01と入力 / 入力しない場合は0:00に通知されます。"),
     plan_msg: Option(str, "メッセージ(メンションも含めます。)"),
-    time: Option(str, "通知時刻を設定できます。(例:12時00分や、22:00なども可能)※半角数字のみ、1などの単数は01と入力 / 入力しない場合は0:00に通知されます。", required=False),
 ):
     """通知する予定を作成します。メンバーと役職の指定ページは、必須の引数を全て入力後、TABキーを押して表示できます。"""
     if time is None:
@@ -65,15 +73,16 @@ async def create(
     else:
         hour, minutes = strptime(time)
     year, month, day = set_date(date)
+    msg = f"<@{mention.id}> {plan_msg}"
     bot.cur.execute("INSERT INTO plan(name, msg, year, month, day, hour, minutes) VALUES(?,?,?,?,?,?,?)",
-                    (plan_name, plan_msg, year, month, day, hour, minutes))
+                    (plan_name, msg, year, month, day, hour, minutes))
     bot.conn.commit()
     embed = discord.Embed(
         title="予定の作成完了", description="次のように予定の作成が完了しました。", color=0x4CAF50)
     embed.add_field(name="予定の名前", value=f"{plan_name}", inline=True)
     embed.add_field(name="通知日", value=f"{year}年{month}月{day}日", inline=True)
     embed.add_field(name="通知時刻", value=f"{hour}時{minutes}分", inline=True)
-    embed.add_field(name="メッセージ内容", value=f"{plan_msg}", inline=True)
+    embed.add_field(name="メッセージ内容", value=f"({mention}へのメンション) {plan_msg}", inline=True)
     await ctx.respond(embed=embed)
 
 
@@ -82,20 +91,19 @@ async def get_plan(ctx: discord.AutocompleteContext):
     options = []
     bot.cur.execute("SELECT * FROM plan;")
     for p in bot.cur.fetchall():
-        print(f"{p[0]}")
         options.append(f"{p[0]}")
     return options
 
 
-@bot.slash_command(guild_ids=[737154037937340506])
+@bot.slash_command(guild_ids=[int(get_token("guild_id"))])
 async def edit(
     ctx,
     plan: Option(str, "予定を選択してください。", autocomplete=get_plan),
-    plan_name: Option(str, "予定の名前"),
+    plan_name: Option(en.SlashCommandOptionType(9), "予定の名前"),
+    mention: Option(str, "メンション先(@を打った後メンション先一覧を表示してください。)"),
     date: Option(str, "日付(例:2022/01/01) ※半角数字のみ、1などの単数は01と入力"),
+    time: Option(str, "通知時刻を設定できます。(例:午後12時00分や、22:00なども可能)※半角数字のみ、1などの単数は01と入力 / 入力しない場合は0:00に通知されます。"),
     plan_msg: Option(str, "メッセージ(メンションも含めます。)"),
-    time: Option(
-        str, "通知時刻を設定できます。(例:12時00分や、22:00なども可能)※半角数字のみ、1などの単数は01と入力 / 入力しない場合は0:00に通知されます。", required=False) = None,
 ):
     """予定の通知時間やメッセージを変更します。"""
     if time is None:
@@ -104,19 +112,20 @@ async def edit(
     else:
         hour, minutes = strptime(time)
     year, month, day = set_date(date)
+    msg = f"<@{mention.id}> {plan_msg}"
     bot.cur.execute("UPDATE plan SET name = ?, msg = ?, year = ?, month = ?, day = ?, hour = ?, minutes = ? WHERE name = ?",
-                    (plan_name, plan_msg, year, month, day, hour, minutes, plan))
+                    (plan_name, msg, year, month, day, hour, minutes, plan))
     bot.conn.commit()
     embed = discord.Embed(
         title="予定の変更完了", description="次のように予定の変更が完了しました。", color=0xFFEB3B)
     embed.add_field(name="予定の名前", value=f"{plan_name}", inline=True)
     embed.add_field(name="通知日", value=f"{year}年{month}月{day}日", inline=True)
     embed.add_field(name="通知時刻", value=f"{hour}時{minutes}分", inline=True)
-    embed.add_field(name="メッセージ内容", value=f"{plan_msg}", inline=True)
+    embed.add_field(name="メッセージ内容", value=f"({mention}へのメンション) {plan_msg}", inline=True)
     await ctx.respond(embed=embed)
 
 
-@bot.slash_command(guild_ids=[737154037937340506], name="delete")
+@bot.slash_command(guild_ids=[int(get_token("guild_id"))], name="delete")
 async def delete_(
     ctx,
     plan: Option(str, "予定を選択してください。", autocomplete=get_plan),
@@ -129,7 +138,7 @@ async def delete_(
     await ctx.respond(embed=embed)
 
 
-@bot.slash_command(guild_ids=[737154037937340506], name="list")
+@bot.slash_command(guild_ids=[int(get_token("guild_id"))], name="list")
 async def list_(ctx):
     """予定の一覧を表示します。"""
     embed = discord.Embed(title="予定の一覧", color=0x00BCD4)
@@ -153,9 +162,9 @@ async def send_notice():
         for i in bot.cur.fetchall():
             embed = discord.Embed(title=i[0], color=0xFDD835, timestamp=datetime.datetime(
                 *i[2:7]) - datetime.timedelta(hours=9))  # データベースにはJSTで入っているので9時間引いてる
-            coros.append(bot.get_channel(928487681804087346).send(
+            coros.append(bot.get_channel(int(get_token("channel_id"))).send(
                 embed=embed))  # メッセージを送信するコルーチンを生成して配列にぶち込む
-            coros.append(bot.get_channel(928487681804087346).send(i[1]))
+            coros.append(bot.get_channel(int(get_token("channel_id"))).send(i[1]))
             # データくんは用済みなので消し消し
             bot.cur.execute(
                 "DELETE FROM plan WHERE year <= ? AND month <= ? AND day <= ? AND hour <= ? AND minutes <= ?", sql_taple)
